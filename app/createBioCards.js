9dffad7be4d395966ff13cd6e17a968de4aed234
Ed Marciniak <ekmarciniak@gmail.com>
2022-12-13 10:41:53 -0600

    Enter notes

--- app/createBioCards.js
+++ app/createBioCards.js
@@ -3,6 +3,15 @@ import people from *./people.js";
 
 console.log(people);
 
+/**
+ *1. Look at the each person one by one (iteration)
+ *2. Check if the person is older than 27 (true/false)
+ */
+
+people.filter((theCurrentPersonThatIamCheckingTheAge4) =>
+theCurrentPersonThatIamCheckingTheAge4.age > 27);
+
+
 
 
 
